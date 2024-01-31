15c7eefaf1b9590211aa8b9d674eff9cebc5b678
yongseok2312 <lyos2312@naver.com>
2024-01-31 16:31:14 +0900

    3일차 수정

--- day03/test21_oop.py
+++ day03/test21_oop.py
@@ -2,5 +2,60 @@
 # date : 20240131
 # desc : 객체지향 클래스 만들기
 
-class Person:
-    pass
+# 클래스(사람이라는 객체를 만들기 위한 청사진) 만들기
+class Person: # 사람 클래스 선언
+    name = ''  # 멤버 변수
+    age = 0
+    gender = ''
+
+    # 생성자 함수(스페셜 함수) 클래스의 객체를 생성할때 동작.
+    # init == initialization(초기화)
+    def __init__(self) -> None:
+        self.name = '홍길동'
+        self.age = 29
+        self.gender = '남'
+
+    # 클래스를 호출할 때 원하는 형태로 변환해서 보여줄 때 사용
+    def __str__(self) -> str:
+        strs = f'커스텀 출력, 이름: {self.name} 객체 생성!'
+        return strs
+
+
+    # 멤버 함수 self 필수
+    def walk(self):
+        print(f'{self.name}이(가) 걷습니다.')
+
+    def stop(self):
+        print(f'{self.name}이(가) 멈춥니다.')
+
+
+
+# 사람 객체 생성, instance(사례, 예제)
+mg = Person()
+es = Person()
+
+# print(type(mg))
+# print(id(mg)) # 다른 객체인지 확인
+# print(id(es))
+
+mg.name = '이용석'  # 객체명.멤버변수 = ...
+mg.age = 27
+mg.gender = '남'    
+
+es.name = '애슐리'
+es.age = 25
+es.gender = '여'    
+
+print(f'mg => 이름: {mg.name}, 나이:{mg.age}, 성별:{mg.gender}')
+print(f'es => 이름: {es.name}, 나이:{es.age}, 성별:{es.gender}')
+
+mg.walk()
+print('1분동안 걷는다.')
+mg.stop()
+
+print('생성자 함수 추가') # 아무런 정의가 없을시 생성자 함수에서의 나온 값이 출력
+gd=Person()
+print(f'gd => 이름: {gd.name}, 나이:{gd.age}, 성별:{gd.gender}')
+
+
+print(gd)
---
+++ day03/test22_car.py
@@ -0,0 +1,55 @@
+# file : test22_car.py
+# date : 20240131
+# desc : 카 클래스 만들기
+
+class car:
+    wheel_num = 4
+    color = 'white'
+    __plate_num = ''
+    company = ''
+    gear_type = ''
+
+    # 전진, 후진, 좌회전, 우회전
+    def moveForward(self):
+        print(f'{self.__plate_num}가 전진합니다')
+
+    def moveBackward(self):
+        print(f'{self.__plate_num}가 후진합니다')
+
+    def moveLeft(self):
+        print(f'{self.__plate_num}가 좌회전합니다')
+
+    def moveRight(self):
+        print(f'{self.__plate_num}가 우회전합니다')
+
+    def __init__(self, __plate_num, company, color, gear) -> None:
+        self.__plate_num = __plate_num
+        self.company = company
+        self.color = color
+        self.gear = gear
+
+    def __str__(self) -> str:
+        return f'제 차는 {self.__plate_num}입니다. {self.color}입니다'
+
+    def getplatenumber(self): # 외부에서는 접근 할 수 없도록 막는 조치
+        return self.__plate_num
+    def setplatenumber(self, new_platenumber):
+        self.__plate_num = new_platenumber
+    
+
+
+sarah = car('1234','현대','화이트','자동')
+# 객체를 생성하는 곳과 객체를 사용하는 곳이 다를 수 있다.
+print(sarah)
+print(f'차 번호는 {sarah.getplatenumber}')
+print(f'차 색상은 {sarah.color}')
+sarah.moveForward()
+
+csuv = car('5464','현대','회색','자동')
+print(csuv)
+
+sarah.__plate_num = '9812' #악의적인 의도를 가지고 변경
+print(sarah)
+
+sarah.setplatenumber('231245')  # 캡슐화!
+print(sarah)
